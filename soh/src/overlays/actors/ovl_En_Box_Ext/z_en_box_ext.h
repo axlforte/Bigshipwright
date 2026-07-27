67970eeb53058903e39645fbff850c9a5fe1faeb
axlforte <87778805+axlforte@users.noreply.github.com>
2026-07-27 12:28:57 -0700

    Rename EnBox to EnBoxExt and update types

--- soh/src/overlays/actors/ovl_En_Box_Ext/z_en_box_ext.h
+++ soh/src/overlays/actors/ovl_En_Box_Ext/z_en_box_ext.h
@@ -1,12 +1,12 @@
-#ifndef Z_EN_BOX_H
-#define Z_EN_BOX_H
+#ifndef Z_EN_BOX_EXT_H
+#define Z_EN_BOX_EXT_H
 
 #include <libultraship/libultra.h>
 #include "global.h"
 
-#define ENBOX_PARAMS(type, itemId, treasureFlag) ((type) << 12 | (itemId) << 5 | (treasureFlag))
-#define ENBOX_TREASURE_FLAG_UNK_MIN 20
-#define ENBOX_TREASURE_FLAG_UNK_MAX 32
+#define ENBOX_EXT_PARAMS(type, itemId, treasureFlag) ((type) << 12 | (itemId) << 5 | (treasureFlag))
+#define ENBOX_EXT_TREASURE_FLAG_UNK_MIN 20
+#define ENBOX_EXT_TREASURE_FLAG_UNK_MAX 32
 
 struct EnBox;
 
@@ -16,21 +16,21 @@ typedef enum {
     /*
     only values 1-11 are used explicitly, other values (like 0) default to another separate behavior
     */
-    /*  0 */ ENBOX_TYPE_BIG_DEFAULT,
-    /*  1 */ ENBOX_TYPE_ROOM_CLEAR_BIG,         // appear on room clear, store temp clear as permanent clear
-    /*  2 */ ENBOX_TYPE_DECORATED_BIG,          // boss key chest, different look, same as ENBOX_TYPE_BIG_DEFAULT otherwise
-    /*  3 */ ENBOX_TYPE_SWITCH_FLAG_FALL_BIG,   // falling, appear on switch flag set
-    /*  4 */ ENBOX_TYPE_4,                      // big, drawn differently
-    /*  5 */ ENBOX_TYPE_SMALL,                  // same as ENBOX_TYPE_BIG_DEFAULT but small
-    /*  6 */ ENBOX_TYPE_6,                      // small, drawn differently
-    /*  7 */ ENBOX_TYPE_ROOM_CLEAR_SMALL,       // use room clear, store temp clear as perm clear
-    /*  8 */ ENBOX_TYPE_SWITCH_FLAG_FALL_SMALL, // falling, appear on switch flag set
-    /*  9 */ ENBOX_TYPE_9,                      // big, has something more to do with player and message context?
-    /* 10 */ ENBOX_TYPE_10,                     // like 9
-    /* 11 */ ENBOX_TYPE_SWITCH_FLAG_BIG         // big, appear on switch flag set
-} EnBoxType;
+    /*  0 */ ENBOX_EXT_TYPE_BIG_DEFAULT,
+    /*  1 */ ENBOX_EXT_TYPE_ROOM_CLEAR_BIG,         // appear on room clear, store temp clear as permanent clear
+    /*  2 */ ENBOX_EXT_TYPE_DECORATED_BIG,          // boss key chest, different look, same as ENBOX_TYPE_BIG_DEFAULT otherwise
+    /*  3 */ ENBOX_EXT_TYPE_SWITCH_FLAG_FALL_BIG,   // falling, appear on switch flag set
+    /*  4 */ ENBOX_EXT_TYPE_4,                      // big, drawn differently
+    /*  5 */ ENBOX_EXT_TYPE_SMALL,                  // same as ENBOX_TYPE_BIG_DEFAULT but small
+    /*  6 */ ENBOX_EXT_TYPE_6,                      // small, drawn differently
+    /*  7 */ ENBOX_EXT_TYPE_ROOM_CLEAR_SMALL,       // use room clear, store temp clear as perm clear
+    /*  8 */ ENBOX_EXT_TYPE_SWITCH_FLAG_FALL_SMALL, // falling, appear on switch flag set
+    /*  9 */ ENBOX_EXT_TYPE_9,                      // big, has something more to do with player and message context?
+    /* 10 */ ENBOX_EXT_TYPE_10,                     // like 9
+    /* 11 */ ENBOX_EXT_TYPE_SWITCH_FLAG_BIG         // big, appear on switch flag set
+} EnBoxExtType;
 
-typedef struct EnBox {
+typedef struct EnBoxExt {
     /* 0x0000 */ DynaPolyActor dyna;
     /* 0x0164 */ SkelAnime skelanime;
     /* 0x01A8 */ s32 unk_1A8; // related to animation delays for types 3 and 8
@@ -49,6 +49,6 @@ typedef struct EnBox {
     /*        */ GetItemEntry getItemEntry; // This is only to determine the Chest Style, randomzier item gives are handled elsewhere
     /*        */ Gfx* boxLidDL;
     /*        */ Gfx* boxBodyDL;
-} EnBox; // size = 0x01FC
+} EnBoxExt; // size = 0x01FC
 
 #endif
